8d0a953bae3edaf46068de4e825f56c2295a463f
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-04-18 05:42:11 +0600

    masum-dental-spa structure done

---
+++ src/pages/HomePage/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Services from "../../Services/Services/Services";
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Services />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Shared/NavigationBar/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <h3>this is navbar</h3>
+    </div>
+  );
+};
+
+export default NavigationBar;
